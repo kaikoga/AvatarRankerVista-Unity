d1be0f59287d36829259adee0c423d3733c769d7
kaikoga <info@mail.kaikoga.net>
2025-12-13 22:41:00 +0900

    Manual Measure from menu because we don't have a proper space yet

--- Editor/Silksprite/AvatarRankerVista/Core/Utils/AvatarReportService.cs
+++ Editor/Silksprite/AvatarRankerVista/Core/Utils/AvatarReportService.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Linq;
 using Ablet.Models.Serialized;
 using Ablet.Repositories;
--- Editor/Silksprite/AvatarRankerVista/View/AvatarRankerVistaMenuItems.cs
+++ Editor/Silksprite/AvatarRankerVista/View/AvatarRankerVistaMenuItems.cs
@@ -1,4 +1,6 @@
+using Ablet.Registries;
 using Silksprite.AvatarRankerVista.Core.Serialized;
+using Silksprite.AvatarRankerVista.Core.Utils;
 using UnityEditor;
 
 namespace Silksprite.AvatarRankerVista.View
@@ -16,11 +18,28 @@ namespace Silksprite.AvatarRankerVista.View
         {
             Menu.SetChecked("Tools/Avatar Tinker Vista/Avatar Ranker Vista - Measure on Build", AvatarRankerSettingsRepository.instance.MeasureOnBuild);
         }
-        
+
         [MenuItem("Tools/Avatar Tinker Vista/Avatar Ranker Vista - Measure on Build", false, 100001)]
         static void MeasureOnBuild()
         {
             AvatarRankerSettingsRepository.instance.MeasureOnBuild = !AvatarRankerSettingsRepository.instance.MeasureOnBuild;
+        }
+
+        [MenuItem("Tools/Avatar Tinker Vista/Avatar Ranker Vista - Manual Measure", true, 100002)]
+        static bool ValidateManualMeasure()
+        {
+            if (Selection.activeGameObject == null)
+            {
+                return false;
+            }
+            var platform = PlatformRegistry.Instance.GuessPlatform(Selection.activeGameObject);
+            return platform != null;
+        }
+        
+        [MenuItem("Tools/Avatar Tinker Vista/Avatar Ranker Vista - Manual Measure", false, 100002)]
+        static void ManualMeasure()
+        {
+            AvatarReportService.MeasureAll(Selection.activeGameObject, false);
         }
     }
 }
