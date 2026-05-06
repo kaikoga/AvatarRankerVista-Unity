76645a06118abf3d43a7a85b930c94a8bd3f5000
kaikoga <info@mail.kaikoga.net>
2026-05-07 03:20:47 +0900

    fix conditional compile

--- Editor/Silksprite/AvatarRankerVista/View/UIElements/GameObjectPopupField.cs
+++ Editor/Silksprite/AvatarRankerVista/View/UIElements/GameObjectPopupField.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
-#if !UNITY_2022_OR_NEWER
+#if !UNITY_2022_3_OR_NEWER
 using UnityEditor.UIElements;
 #endif
 
 namespace Silksprite.AvatarRankerVista.View.UIElements
 {
 #if UNITY_2023_2_OR_NEWER
-    [UxmlElement] partial 
+    [UxmlElement] sealed partial 
+#else
+    sealed
 #endif
     class GameObjectPopupField : PopupField<GameObject>
     {
